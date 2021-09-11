7ead8fba26ebcdfcd41381c99813d50a4ea5f9c3
Shreya Bhagat <shreya.bhagat10@gmail.com>
2021-09-11 20:10:51 +0530

    Day3

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:sampleproject/HomePage.dart';
+import 'package:sampleproject/Pages/home_page.dart';
+import 'package:sampleproject/Pages/login_page.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyApp(),
-  ));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return HomePage();
-  }
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.deepOrange),
+      themeMode: ThemeMode.light,
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => const LoginPage(),
+        "/home": (context) => const HomePage(),
+      },
+    ),
+  );
 }
