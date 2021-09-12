f1fa41e9cbb5944d98e0f091a4613661da04475d
Shreya Bhagat <shreya.bhagat10@gmail.com>
2021-09-12 16:05:04 +0530

    Day5

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -2,68 +2,132 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sampleproject/Pages/Home_Page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = " ";
+  bool changeButton = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHomePage(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(const Duration(seconds: 1));
+      await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomePage(),
+        ),
+      );
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text('Hello'),
-          Image.asset(
-            'images/login.png',
-            fit: BoxFit.fill,
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          const Text(
-            'Welcome',
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Enter Username',
-                    labelText: 'Username',
-                  ),
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'images/login.png',
+                fit: BoxFit.fill,
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'Welcome $name',
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
                 ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: 'Enter Password',
-                    labelText: 'Password',
-                  ),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                MaterialButton(
-                    animationDuration: const Duration(seconds: 2),
-                    textColor: Colors.white,
-                    color: Colors.lightBlueAccent.shade700,
-                    child: const Text('Login'),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const HomePage(),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
+                      decoration: const InputDecoration(
+                        hintText: 'Enter Username',
+                        labelText: 'Username',
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Username cannot be empty';
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter Password',
+                        labelText: 'Password',
+                      ),
+                      validator: (var value) {
+                        if (value!.isEmpty) {
+                          return 'Password cannot be empty';
+                        } else if (value.length < 6) {
+                          return 'Password length should be at least 6';
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 30.0,
+                    ),
+                    Material(
+                      color: Colors.deepPurple,
+                      borderRadius:
+                          BorderRadius.circular(changeButton ? 50 : 8),
+                      child: InkWell(
+                        onTap: () => moveToHomePage(context),
+                        child: AnimatedContainer(
+                          duration: const Duration(seconds: 2),
+                          alignment: Alignment.center,
+                          height: 40,
+                          width: changeButton ? 50 : 130,
+                          child: changeButton
+                              ? const Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                )
+                              : const Text(
+                                  'Login',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
+                                ),
                         ),
-                      );
-                    })
-              ],
-            ),
-          )
-        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
